43ebb4332be7af0751f11752e299c488cccd2075
Gustavo Ferreira <53050800+Gustavowp8@users.noreply.github.com>
2022-11-08 11:38:07 -0800

    Add files via upload

---
+++ TesteApp/Produto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteApp
+{
+    internal class Produto
+    {
+        private string Nome;
+
+        public string nome
+        {
+            get { return Nome; }
+            set
+            {
+                if (value.Length > 1)
+                    Nome = value;
+                else
+                    throw new Exception("Nome do produto deve ter pelo menos 2 caracteres");
+            }
+        }
+
+        public double Preco { get; set; }
+
+        public int Estoque { get; private set; }
+
+        public Produto()
+        {
+            this.Estoque = 0;
+        }
+
+        public Produto(string nome, double preco)
+        {
+            this.nome = nome;
+            this.Preco = preco;
+            this.Estoque = 0;
+        }
+
+        public int Vender(int qtde)
+        {
+            if(this.Estoque - qtde >= 0)
+                this.Estoque -= qtde;
+            return this.Estoque;
+        }
+
+        public int Comprar(int qtde)
+        {
+            this.Estoque += qtde;
+            return this.Estoque;
+        }
+
+        public string ObterTexto()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"\nNome: {this.nome}\n");
+            sb.Append($"Preço: {this.Preco}\n");
+            sb.Append($"Estoque: {this.Estoque}\n");
+            return sb.ToString();
+            //Aula05
+        }
+    }
+}
---
+++ TesteApp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TesteApp
+{
+    class Program
+    {
+
+        private static List<Produto> produtos = new List<Produto>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste de função");
+
+            Thread.Sleep(1000);
+
+
+            Opc();
+        }
+        static void Opc()
+        {
+            Console.WriteLine("Classes, Objetos e Escopos de Visibilidade");
+
+            string comando = "S";
+
+            do
+            {
+                //Exibir Menu
+                Console.Clear();
+                Console.WriteLine("Escolha um produto");
+                Console.WriteLine("1 - Cadstra Produtos");
+                Console.WriteLine("2 - Ver lista de produtos");
+                Console.WriteLine("S - Sair");
+
+                //leitura do comando do usuario
+                Console.Write("Opção desejada: ");
+                comando = Console.ReadKey().KeyChar.ToString().ToUpper();
+
+                switch (comando)
+                {
+                    case "1":
+                        Console.Write("\nNome do Produto: ");
+                        string nome = Console.ReadLine();
+                        Console.Write("Preço do produto: ");
+                        string preco = Console.ReadLine();
+                        Produto novoProduto = new Produto(nome, Convert.ToDouble(preco));
+                        produtos.Add(novoProduto);
+                        Console.WriteLine("Produto adicionado com sucesso");
+
+                        Console.ReadKey();
+
+                        Opc();
+
+                        break;
+
+                    case "2":
+                        if (produtos.Count > 0)
+                        {
+                            Console.WriteLine("\nListagem de produtos");
+                            foreach (Produto p in produtos)
+                            {
+                                Console.WriteLine(p.ObterTexto());
+                            }
+                            Console.WriteLine("Precione qualquer tecla para prosseguir...");
+                            Console.ReadKey();
+                        }
+                        else
+                            Console.WriteLine("\nNão há produtos cadastrados!");
+                        Console.ReadKey();
+
+                        Opc();
+                        break;
+                    case "S":
+                        Console.WriteLine("\nObrigado por usar o programa");
+                        break;
+                    default:
+                        Console.WriteLine("Opção invalida tente novamente");
+                        Console.ReadKey();
+
+                        Opc();
+                        break;
+                }
+
+
+            } while (false);
+        }
+
+
+    }
+}
